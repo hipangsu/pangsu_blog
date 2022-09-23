c388e9a7cd028bd6aa181ffd0bb49bd831e42268
hipangu <hipangsu@gmail.com>
2022-09-23 17:55:15 +0900

    1

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,4 +1,33 @@
-(function (win, doc, undefined) {
 
-    console.log("ez");
-}) (window, document);
+const accordion = document.querySelectorAll('.js_acc_hd');
+
+for(let i=0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function() { 
+        nextNode = this.nextElementSibling;
+
+        if(!nextNode.classList.contains('active')) {
+            nextNode.classList.add('active')
+        } else {
+            nextNode.classList.remove('active');
+        }
+
+    })
+}
+
+const html = document.querySelector('html');
+const mode = document.querySelector('.js_mode');
+
+function modeHandler() {
+    html.classList.toggle('mode_dark');
+}
+
+mode.addEventListener('click', modeHandler);
+
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+console.log(today);
