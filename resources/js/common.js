3277bb3a330f5a8526aae90a453cdb925bf5fdf7
hipangu <hipangsu@gmail.com>
2022-09-27 17:53:36 +0900

    1

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,54 +1,36 @@
+// const accordion = document.querySelectorAll('.js_acc_hd');
 
-const accordion = document.querySelectorAll('.js_acc_hd');
+// for(let i=0; i<accordion.length; i++) {
+//     accordion[i].addEventListener('click', function() { 
+//         nextNode = this.nextElementSibling;
 
-for(let i=0; i<accordion.length; i++) {
-    accordion[i].addEventListener('click', function() { 
-        nextNode = this.nextElementSibling;
+//         if(!nextNode.classList.contains('active')) {
+//             nextNode.classList.add('active')
+//         } else {
+//             nextNode.classList.remove('active');
+//         }
 
-        if(!nextNode.classList.contains('active')) {
-            nextNode.classList.add('active')
-        } else {
-            nextNode.classList.remove('active');
-        }
+//     })
+// }
 
-    })
+// ;(function(win, doc, undefined) { 
+
+// })(window, document);
+
+const mode = {
+
+    click: function() {
+        // const html = document.querySelector('html');
+        
+        console.log('ezzzz');
+    },
+
+    init: function() {
+        const modeBtn = document.querySelector('.js_mode')
+
+        modeBtn.addEventListener('click');
+    }
 }
 
-const html = document.querySelector('html');
-const mode = document.querySelector('.js_mode');
 
-function modeHandler() {
-    html.classList.toggle('mode_dark');
-}
-
-mode.addEventListener('click', modeHandler);
-
-
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-console.log(today);
-
-
-
-let sum = (a, b) => {
-    let result = a + b;
-    return result;
-};
-
-alert(sum(1,2));
-
-const numbers = ["1", "2", "3", "4", "5"];
-
-for(let i =0; i<numbers.length; i++) {
-    console.log('ez');
-}
-
-numbers.forEach(function() {
-    console.log('ez');
-});
-
-
+mode.init();
