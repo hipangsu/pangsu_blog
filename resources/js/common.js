975b30b4a35972920561ce87f6a1e89d05fb9405
hipangu <hipangsu@gmail.com>
2022-10-05 18:00:14 +0900

    1

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,21 +1,21 @@
-// const accordion = document.querySelectorAll('.js_acc_hd');
-
-// for(let i=0; i<accordion.length; i++) {
-//     accordion[i].addEventListener('click', function() { 
-//         nextNode = this.nextElementSibling;
-
-//         if(!nextNode.classList.contains('active')) {
-//             nextNode.classList.add('active')
-//         } else {
-//             nextNode.classList.remove('active');
-//         }
-
-//     })
-// }
-
-// ;(function(win, doc, undefined) { 
-
-// })(window, document);
 
 
+const mode = {
+    target: document.querySelector('.js_mode'),
 
+    click: function() {
+        const html = document.querySelector('html');
+
+        // console.log(html);
+        html.classList.toggle('mode_dark');
+    },
+
+    init: function() {
+        mode.target.addEventListener('click', function() {
+
+        mode.click();
+        })
+    }
+}
+
+mode.init();
