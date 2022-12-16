bf031cedca3b44a7067564ed5ded4c263b7961ab
hipangu <hipangsu@gmail.com>
2022-12-16 18:00:27 +0900

    1

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,48 +1,11 @@
-
-
 ;(function(){
-    window.common = {};
 
-    common = {
+    tab = function() {
+        const tab = {
+            tabMenu : document.querySelector('tabMenu li'),
+            tabCont : document.querySelectorAll('.tabCont .cont'),
+            
+        };
 
-        mode: {
-            target: document.querySelector('.js_mode'),
-
-            mode_act: function() {
-                const html = document.querySelector('html');
-    
-                html.classList.toggle('mode_dark');
-            },
-            init: function() {
-                const o = this;
-
-                o.target.addEventListener('click', function() {
-                    o.mode_act();
-                    console.log(this);
-                })
-                // this.target.addEventListener('click', () => this.mode_act());  
-            },
-        },
-
-        ex : {
-            target: null,
-
-            ex_act: function() {
-                console.log('hello');
-            },
-            init: function() {
-                const o = this;
-
-                o.ex_act();
-            }
-
-        },
-
-    }
-
-    document.addEventListener('DOMContentLoaded', function(){
-        common.mode.init();
-        common.ex.init();
-    });
-    
+    };
 })(window, document);
