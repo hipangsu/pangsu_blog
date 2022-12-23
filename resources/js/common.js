9eb1bf1980024fbb55d3af509bbc0c44499c72f5
hipangu <hipangsu@gmail.com>
2022-12-23 15:59:12 +0900

    1

--- resources/js/common.js
+++ resources/js/common.js
@@ -8,4 +8,47 @@
         };
 
     };
+
+    accordion = function() {
+        const acc = {
+            accBox : document.querySelector(".acco-container"),
+            accBtn : document.querySelector(".acco-header"),
+        }
+        console.log();
+    }
+
 })(window, document);
+
+
+
+
+const acco = {
+    target: null,
+
+    click : function(v) {
+        const o = this;
+
+        o.accHeader = v.parentNode;
+        o.accCont = o.accHeader.nextElementSibling;
+        o.accCont.classList.toggle('active');
+        console.log("ez");
+    },
+
+    init: function() {
+        const o = this;
+        o.target = document.getElementsByClassName('acco-header');
+
+        for(const accBtn of o.target) {
+            accBtn.addEventListener('click', function() {
+                o.click(this);
+                console.log("ez");
+            });
+        }
+    }
+}
+acco.init();
+
+
+
+
+
