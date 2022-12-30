d25c175821d8bae97155ae9d7f7bce30b71f0e03
hipangu <hipangsu@gmail.com>
2022-12-30 15:27:47 +0900

    1

---
+++ resources/js/app.js
@@ -0,0 +1,22 @@
+const player = {
+    name: "pangsu",
+    points: 10,
+
+}
+
+console.log(player);
+console.log(player.points);
+
+const objects = {
+    target: document.querySelector(),
+
+
+    event : function() {
+
+    },
+
+
+    init: function() {
+
+    },
+}
